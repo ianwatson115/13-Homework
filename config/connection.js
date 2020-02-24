b325a9e71aec853cf3585e2f1263badc06ebc544
ian <ianwatson115@gmail.com>
2020-02-24 10:42:56 -0500

    copied activity connection code and refactored it.

---
+++ config/connection.js
@@ -0,0 +1,21 @@
+// Set up MySQL connection.
+const mysql = require("mysql");
+
+const connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "rootroot",
+  database: "burgers_db"
+});
+
+// Make connection.
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+  console.log("connected as id " + connection.threadId);
+});
+
+module.exports = connection;
