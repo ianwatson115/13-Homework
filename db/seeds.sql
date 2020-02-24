f2d751608b4f066324e48841e4ed8a60162f3cd2
ian <ianwatson115@gmail.com>
2020-02-24 10:39:24 -0500

    added a server file that requires express. created a db folder to hold mysql entries.

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) values ("The Big Poppa", false);
+INSERT INTO burgers (burger_name, devoured) values ("Fat Burger", false);
+INSERT INTO burgers (burger_name, devoured) values ("Anti Plant-based", false);
